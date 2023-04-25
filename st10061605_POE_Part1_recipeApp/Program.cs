8ce65c5a56cb1f01a31c7c193b656a11e5f744c2
Vaseline BlueSeal <Vaseline BlueSeal@THERIG>
2023-04-25 12:43:20 +0200

    Add project files.

---
+++ st10061605_POE_Part1_recipeApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace st10061605_POE_Part1_recipeApp
+{   
+    class Recipe
+    {
+        //declarations
+        private int numIngredients;
+        private string[] ingredients; //storing these in a array
+        private double[] quantiites;
+        private string[] units;
+        private int numSteps;
+        private string[] theSteps;
+
+        public Recipe()
+        {
+            //intializing the variables
+            numIngredients = 0;
+            ingredients = new string[0];
+            quantiites = new double[0];
+            units = new string[0];
+            numSteps = 0;
+            theSteps = new string[0];
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
